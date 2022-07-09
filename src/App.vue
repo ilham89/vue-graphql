4e2097c1c5c4bedfb050a1dfce2a17a84c9e579e
ilham89 <ilham.kodingworks@gmail.com>
2022-07-09 13:35:49 +0700

    query with graphql

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <input type="text" v-model="searchTerm" />
+    <p v-if="loading">Loading...</p>
+    <p v-else-if="error">Something went wrong! Please try again</p>
+    <template v-else>
+      <p v-for="book in result.allBooks" :key="book.id">
+        {{ book.title }}
+      </p>
+    </template>
+  </div>
+</template>
+
+
+<script>
+import { useQuery } from "@vue/apollo-composable";
+import ALL_BOOKS_QUERY from "./graphql/allBooks.query.gql";
+import { ref } from "vue";
+
+export default {
+  name: "App",
+  setup() {
+    const searchTerm = ref(""); // create a reactive property with 'ref'
+
+    const { result, loading, error } = useQuery(ALL_BOOKS_QUERY, () => ({
+      search: searchTerm.value,
+    }));
+
+    return { result, searchTerm, loading, error };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.list-wrapper {
+  display: flex;
+  margin: 0 auto;
+  max-width: 960px;
+}
+
+.list {
+  width: 50%;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { createApp, h, provide } from "vue";
+import App from "./App.vue";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+  render: () => h(App),
+});
+
+app.mount("#app");
